374b8751644bae24460d2ea964d507ae059b69ac
ReyesRamirez <a.22308051281289@cbtis128.edu.mx>
2024-09-20 09:30:07 -0600

    Add files via upload

---
+++ operadoresA.py
@@ -0,0 +1,45 @@
+print("Act 9 clases v2")
+print("Reyes Ramirez: 22308051281289\n")
+# zona de class
+class Operadores1289:
+    #zona de funciones
+    def suma1289(self,r,R):
+        s1289=r+R
+        print(f"La suma de {r} + {R} = {s1289}\n")
+    def resta1289(self,r,R):
+        r1289=r-R
+        print(f"La resta de {r} - {R} = {r1289}\n")
+    def Multiplicacion1289(self,r,R):
+        m1289=r*R
+        print(f"La multiplicacion de {r} * {R} = {m1289}\n")
+    def Divicion1289(self,r,R):
+        d1289=r/R
+        print(f"La division de {r} / {R} = {d1289}\n")
+    def Modulo1289(self,r,R):
+        mo1289=r%R
+        print(f"El modulo de {r} % {R} = {mo1289}\n")
+    def Exponente1289(self,r,R):
+        e1289=r**R
+        print(f"El exponente de {r} ** {R} = {e1289}\n")
+    def Cociente1289(self,r,R):
+        c1289=r//R
+        print(f"El cociente de {r} // {R} = {c1289}\n")
+
+# zona de creacion del objeto
+operaReyes=Operadores1289()
+
+# zona de uso de objetos
+print(" Funcion suma")
+operaReyes.suma1289(5,4)
+print(" Funcion resta")
+operaReyes.resta1289(8,2)
+print(" Funcion multiplicacion")
+operaReyes.Multiplicacion1289(7,7)
+print(" Funcion division")
+operaReyes.Divicion1289(3,2)
+print(" Funcion modulo")
+operaReyes.Modulo1289(9,4)
+print(" Funcion exponente")
+operaReyes.Exponente1289(5,3)
+print(" Funcion cociente")
+operaReyes.Cociente1289(7,3)
